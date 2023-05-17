df25a83c3823f61790ecd7fa7b09c2d958a44e39
Coffee-crocodile <1147347984@qq.com>
2023-05-17 11:55:14 +0800

    feat: add docs config

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -2,27 +2,69 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Zeal UI docs",
-  description: "Zeal UI docs",
+  title: "Zeal UI",
+  titleTemplate: 'Zeal',
+  description: "Zeal UI docs site",
+  // 打包目录
+  outDir: '../dist',
+
+  srcDir: './src',
+  head: [
+    // 添加图标
+    ['link', { rel: 'icon', href: '/zeal-ui.svg' }]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+
+    search: {
+      provider: 'local'
+    },
+    // 获取每个文件最后一次 git 提交的 UNIX 时间戳(ms)，同时它将以合适的日期格式显示在每一页的底部
+    lastUpdatedText: 'Last Updated', // string | boolean
+    logo: '/zeal-ui.svg',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/guide/introduce' },
+      { text: '组件', link: '/components/button' },
+      { text: '演练场', link: 'https://play.vuejs.org' },
+      {
+        text: '0.0.1', items: [
+          { text: 'issue', link: 'https://github.com/chansee97/zeal-ui/issues' },
+      ] },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/guide/': [
+        {
+          text: '介绍',
+          items: [
+            { text: 'Zeal', link: '/guide/introduce' },
+          ]
+        },
+         {
+          text: '快速开始',
+          items: [
+            { text: '安装', link: '/guide/start' },
+          ]
+        }
+      ],
+      '/components/': [
+        {
+          text: 'components',
+          items: [
+            { text: 'Button', link: '/components/button' },
+          ]
+        }
+      ]
+    },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/chansee97/zeal-ui' }
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present Rock Chen'
+    }
   }
 })
---
+++ packages/docs/src/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Zeal UI"
+  text: "欢迎来到 Zeal UI"
+  tagline: Zeal 是一套基于 Vue.js 的开源 UI 组件库，主要服务于 PC 界面的中后台产品。
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: /guide/introduce
+    - theme: alt
+      text: 组件文档
+      link: /components/button
+
+features:
+  - title: 全面拥抱 Composition API
+    details: 从源码到文档
+  - title: TypeScript
+    details: 完全使用 TypeScript 开发，提供完整的类型定义
+  - title: Monorepo
+    details: Monorepo 管理模式：docs, components, hooks
+---
+
